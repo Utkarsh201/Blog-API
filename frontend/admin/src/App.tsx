3f6946c8c7304ee77c1780ce18f4098d13978991
Utkarsh Gupta <utkarsh3333gupta@gmail.com>
2026-07-14 16:42:33 +0530

    admin login and signup page done

--- frontend/admin/src/App.tsx
+++ frontend/admin/src/App.tsx
@@ -1,6 +1,21 @@
+import { useState, useEffect } from 'react'
+import type {Dispatch, SetActionState} from 'react'
+import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom'
+
 
 function App() {
-  return <></>
+  return (
+    <></>
+  );
 }
 
-export default App
+function AppWrapper(){
+  return (
+    <Router basename = '/admin'>
+      <App/>
+    </Router>
+  )
+}
+
+
+export default AppWrapper
