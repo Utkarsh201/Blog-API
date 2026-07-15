f690aff7f5bd83d33ea636a3d8825fd3dd67c4cd
Utkarsh Gupta <utkarsh3333gupta@gmail.com>
2026-07-15 22:53:46 +0530

    admin frontend done

--- frontend/admin/src/App.tsx
+++ frontend/admin/src/App.tsx
@@ -1,11 +1,85 @@
 import { useState, useEffect } from 'react'
-import type {Dispatch, SetActionState} from 'react'
+import type {Dispatch, SetStateAction} from 'react'
 import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom'
+import SignupForm from './pages/SignupForm';
+import Dashboard from './pages/DashBoard';
+import LoginForm from './pages/LoginForm';
+import CreateBlog from './pages/CreateBlog';
+
+
+type SetTokenType = Dispatch<SetStateAction<string | null>>;
+
+const LoginWithProps = ({setToken} : {setToken : SetTokenType}) => (
+  <LoginForm setToken = {setToken}/>
+);
+
+const SignupFormWithProps  = ({setToken}: {setToken : SetTokenType}) => (
+  <SignupForm setToken = {setToken}/>
+);
 
 
 function App() {
+
+  const [token, setToken] = useState<String | null>(null);
+
+  useEffect(()=>{
+    const storedToken = localStorage.getItem('adminToken');
+    setToken(storedToken); 
+  }, [])
+
   return (
-    <></>
+    <Routes>
+      <Route 
+        path="/login" 
+        element={
+          token ? (
+            <Navigate to="/dashboard" replace />
+          ) : (
+            <LoginWithProps setToken={setToken} />
+          )
+        } 
+      />
+      <Route 
+        path="/signup" 
+        element={
+          token ? (
+            <Navigate to="/dashboard" replace />
+          ) : (
+            <SignupFormWithProps setToken={setToken} />
+          )
+        } 
+      />
+      <Route 
+        path="/dashboard" 
+        element={
+          token ? (
+            <Dashboard />
+          ) : (
+            <Navigate to="/signup" replace />
+          )
+        } 
+      />
+      <Route 
+        path="/create-blog" 
+        element={
+          token ? (
+            <CreateBlog />
+          ) : (
+            <Navigate to="/signup" replace />
+          )
+        } 
+      />
+      <Route 
+        path="/" 
+        element={
+          token ? (
+            <Navigate to="/dashboard" replace />
+          ) : (
+            <Navigate to="/signup" replace />
+          )
+        } 
+      />
+    </Routes>
   );
 }
 
